6ac5807563698df40a9e461c44cf1b479daab826
nakul1010 <nakul.amate@gmail.com>
2022-05-15 23:44:12 +0530

    fix: errors

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,56 +1,45 @@
-pub fn hello_nakul()
-{
+pub fn hello_nakul() {
     println!("Hello, Nakul!");
 }
 
-pub fn hello_yash()
-{
+pub fn hello_yash() {
     println!("Hello, Yash!");
 }
 
-pub fn hello_raj()
-{
+pub fn hello_raj() {
     println!("Hello, Raj!");
 }
 
-pub fn hello_alice()
-{
+pub fn hello_alice() {
     println!("Hello, Alice!");
 }
 
-
-pub fn hello_raj()
-{
-    println!("Hello, Raj!");
-}
-
-pub fn hello_shounak()
-{
+pub fn hello_shounak() {
     println!("Hello, Shounak!");
-    for i in 0..100{
+    for i in 0..100 {
         println!("1");
     }
     println!("Hello, Shounak!");
-    for i in 0..100{
+    for i in 0..100 {
         println!("1");
     }
 }
 
-pub fn add_function(no1:i32,no2:i32){
-    println!("Addition : {}",no1+no2);
+pub fn add_function(no1: i32, no2: i32) {
+    println!("Addition : {}", no1 + no2);
 }
-pub fn sub_function(no1:i32,no2:i32){
-    println!("Sub : {}",no1-no2);
+pub fn sub_function(no1: i32, no2: i32) {
+    println!("Sub : {}", no1 - no2);
 }
-pub fn mul_function(no1:i32,no2:i32){
-    println!("Sub : {}",no1**no2);
-pub fn after_rebase()
-{
+pub fn mul_function(no1: i32, no2: i32) {
+    println!("Sub : {}", no1 * no2);
+}
+pub fn after_rebase() {
     println!("After Rebase");
 }
 
 fn main() {
     hello_nakul();
-    add_function(3 as i32,3 as i32);
-    sub_function(1 as i32,3 as i32);
+    add_function(3 as i32, 3 as i32);
+    sub_function(1 as i32, 3 as i32);
 }
