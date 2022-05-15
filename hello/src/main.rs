7dc6b96afe9b279be5983d226881636798a06fea
nakul1010 <nakul.amate@gmail.com>
2022-05-15 20:14:50 +0530

    2 more hello function adeed

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,6 +3,17 @@ pub fn hello_nakul()
     println!("Hello, Nakul!");
 }
 
+pub fn hello_yash()
+{
+    println!("Hello, Yash!");
+}
+
+
+pub fn hello_shounak()
+{
+    println!("Hello, Shounak!");
+}
+
 fn main() {
     hello_nakul();
 }
