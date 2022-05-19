a541a168a67161b3d03efaec050e0de2525c6527
nakul1010 <nakul.amate@gmail.com>
2022-05-19 11:05:39 +0530

    2 calcis

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,29 @@
 
-pub fn calculator() {
+pub fn calculator_1() {
+    println!("Enter nos plus operator ");
+    let n1 = 100;
+    let n2 = 200;
+    let operator = String::from("+");
+
+    if operator == "+".to_string(){
+        println!("{} + {} = {}",n1,n2,n1+n2);
+    }
+    
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
+    }
+
+    else if operator == "*".to_string(){
+        println!("{} * {} = {}",n1,n2,n1*n2);
+    }
+
+    else if operator == "/".to_string(){
+        println!("{} / {} = {}",n1,n2,n1/n2);
+    }
+}
+
+
+pub fn calculator_2() {
     println!("Enter nos plus operator ");
     let n1 = 100;
     let n2 = 200;
