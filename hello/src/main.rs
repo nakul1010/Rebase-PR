4d6495a94236fb3faa4d8bca1ee9b40bdf19bfc9
nakul1010 <nakul.amate@gmail.com>
2022-05-19 11:07:01 +0530

    4 calcis

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -46,6 +46,56 @@ pub fn calculator_2() {
     }
 }
 
+
+
+pub fn calculator_3() {
+    println!("Enter nos plus operator ");
+    let n1 = 100;
+    let n2 = 200;
+    let operator = String::from("+");
+
+    if operator == "+".to_string(){
+        println!("{} + {} = {}",n1,n2,n1+n2);
+    }
+    
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
+    }
+
+    else if operator == "*".to_string(){
+        println!("{} * {} = {}",n1,n2,n1*n2);
+    }
+
+    else if operator == "/".to_string(){
+        println!("{} / {} = {}",n1,n2,n1/n2);
+    }
+}
+
+
+
+pub fn calculator_4() {
+    println!("Enter nos plus operator ");
+    let n1 = 100;
+    let n2 = 200;
+    let operator = String::from("+");
+
+    if operator == "+".to_string(){
+        println!("{} + {} = {}",n1,n2,n1+n2);
+    }
+    
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
+    }
+
+    else if operator == "*".to_string(){
+        println!("{} * {} = {}",n1,n2,n1*n2);
+    }
+
+    else if operator == "/".to_string(){
+        println!("{} / {} = {}",n1,n2,n1/n2);
+    }
+}
+
 fn main() {
     calculator();
 }
