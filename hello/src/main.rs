3e5c5825450759d5f8b2a46c3d0b280c724d33df
nakul1010 <nakul.amate@gmail.com>
2022-05-23 23:04:12 +0530

    Add multiplication and division

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -30,25 +30,6 @@ pub fn hello_world() {
     println!("Hello World");
 }
 
-<<<<<<< HEAD
-pub fn addition_of_two_nos(n1:i32,n2:i32)
-{
-    println!("{} + {} = {}",n1,n2,n1+n2);
-}
-
-
-pub fn subtract_of_two_nos(n1:i32,n2:i32)
-{
-    println!("{} - {} = {}",n1,n2,n1-n2);
-}
-
-pub fn add_of_three_nos(n1:i32,n2:i32,n3:i32)
-{
-    println!("{} + {} + {} = {}",n1,n2,n3,n1+n2+n3);
-}
-
-=======
->>>>>>> Add multiplication and division
 pub fn multiply_of_two_nos(n1:i32,n2:i32)
 {
     println!("{} * {} = {}",n1,n2,n1*n2);
