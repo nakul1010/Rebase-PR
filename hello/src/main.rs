e199ffbe6ecd6a208f16302e12eb4dbd1a2d5c6b
nakul1010 <nakul.amate@gmail.com>
2022-05-23 23:13:36 +0530

    multiply 3 nos

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -35,6 +35,10 @@ pub fn multiply_of_two_nos(n1:i32,n2:i32)
     println!("{} * {} = {}",n1,n2,n1*n2);
 }
 
+pub fn multiply_of_three_nos(n1:i32,n2:i32,n3:i32)
+{
+    println!("{} * {} * {} = {}",n1,n2,n3,n1*n2*n3);
+}
 
 pub fn divide_of_two_nos(n1:i32,n2:i32)
 {
