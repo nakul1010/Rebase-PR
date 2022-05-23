b32da475c04262b60164c0218effe8a353d703af
nakul1010 <nakul.amate@gmail.com>
2022-05-23 23:04:12 +0530

    Add multiplication and division

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -13,8 +13,8 @@ pub fn calculator() {
         println!("{} * {} = {}",n1,n2,n1*n2);
     }
 
-    else if operator == "*".to_string(){
-        println!("{} * {} = {}",n1,n2,n1*n2);
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
     }
 
     else if operator == "/".to_string(){
@@ -38,6 +38,16 @@ pub fn subtract_of_two_nos(n1:i32,n2:i32)
 }
 
 
+pub fn multiply_of_two_nos(n1:i32,n2:i32)
+{
+    println!("{} * {} = {}",n1,n2,n1*n2);
+}
+
+
+pub fn divide_of_two_nos(n1:i32,n2:i32)
+{
+    println!("{} / {} = {}",n1,n2,n1/n2);
+}
 fn main() {
     calculator();
 }
