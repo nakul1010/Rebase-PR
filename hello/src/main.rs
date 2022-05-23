325b379d6ec4851a990f36f89d5ae76c2e0f610e
nakul1010 <nakul.amate@gmail.com>
2022-05-23 23:12:01 +0530

    sub 3 nos function

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -42,6 +42,10 @@ pub fn add_of_three_nos(n1:i32,n2:i32,n3:i32)
     println!("{} + {} + {} = {}",n1,n2,n3,n1+n2+n3);
 }
 
+pub fn sub_of_three_nos(n1:i32,n2:i32,n3:i32)
+{
+    println!("{} - {} - {} = {}",n1,n2,n3,n1-n2-n3);
+}
 
 fn main() {
     calculator();
