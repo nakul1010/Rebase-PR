3ce96481c8a8ef432bd2824282ad5409db9976b6
nakul1010 <nakul.amate@gmail.com>
2022-05-26 19:51:22 +0530

    changes added

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -9,8 +9,12 @@ pub fn calculator() {
         println!("{} + {} = {}",n1,n2,n1+n2);
     }
     
-    else if operator == "*".to_string(){
-        println!("{} * {} = {}",n1,n2,n1*n2);
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
+    }
+
+    else if operator == "-".to_string(){
+        println!("{} - {} = {}",n1,n2,n1-n2);
     }
 
     else if operator == "-".to_string(){
@@ -26,6 +30,7 @@ pub fn hello_world() {
     println!("Hello World");
 }
 
+<<<<<<< HEAD
 pub fn addition_of_two_nos(n1:i32,n2:i32)
 {
     println!("{} + {} = {}",n1,n2,n1+n2);
@@ -42,6 +47,8 @@ pub fn add_of_three_nos(n1:i32,n2:i32,n3:i32)
     println!("{} + {} + {} = {}",n1,n2,n3,n1+n2+n3);
 }
 
+=======
+>>>>>>> Add multiplication and division
 pub fn multiply_of_two_nos(n1:i32,n2:i32)
 {
     println!("{} * {} = {}",n1,n2,n1*n2);
